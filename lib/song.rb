ee3ad3f8ab3226154bb031748a331ecb4153a261
Neil Seejoor <nvseejoor@gmail.com>
2018-04-02 18:51:41 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,7 +11,7 @@ class Song
 
   def self.column_names
     DB[:conn].results_as_hash = true
-    binding.pry
+
     sql = "pragma table_info('#{table_name}')"
 
     table_info = DB[:conn].execute(sql)
@@ -27,6 +27,7 @@ class Song
   end
 
   def initialize(options={})
+    binding.pry
     options.each do |property, value|
       self.send("#{property}=", value)
     end
